96b43b5d7e8364baee7b0bfe701d08c754256c9e
Alexandre Lubeck <alexandrelubeck@gmail.com>
2024-03-02 09:30:52 -0300

    alterações na msg de error

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 const form = document.getElementById('formulario');
+let valorA, valorB;
 
 function validaNumero() {
-  let valorA = parseFloat(document.getElementById('na').value);
-  let valorB = parseFloat(document.getElementById('nb').value);
+  valorA = parseFloat(document.getElementById('na').value);
+  valorB = parseFloat(document.getElementById('nb').value);
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    validaNumero();
 
     if (valorA > valorB) {
-    alert("O valor de A é maior que o valor de B.");
+    document.querySelector('.msg_erro').style.display = 'block';
+    valorA.value = '';
+    valorB.value = '';
   } else if (valorB > valorA) {
     alert("O valor de B é maior que o valor de A.");
   } else {
     alert("Os valores de A e B são iguais.");
   }
 })
+
+validaNumero.addEventListener('keyup', function(e) {
+  form = validaNumero(e.target.value);
+
+  if (valorA > valorB) {
+    document.querySelector('.msg_erro').style.display = 'block';
+  } else if (valorB > valorA) {
+    document.querySelector('.msg_erro').style.display = 'none';
+  } else {
+    alert("Os valores de A e B são iguais.");
+  }
+});
